407315ad179edeba6118e5d1225c6f239cd9239c
harkamal-react <harkamal.singh@itmtb.com>
2023-03-11 18:15:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Paper } from "@mui/material";
+import HorizontalLinearStepper from "./components/MainForm";
+import store  from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<div className="main_Container">
+				<Provider store={store}>
+					<Paper>
+						<HorizontalLinearStepper />
+					</Paper>
+				</Provider>
+			</div>
+		</div>
+	);
 }
 
 export default App;
